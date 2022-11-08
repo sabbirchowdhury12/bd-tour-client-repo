abaece64497814bec5919b2566b94d6e66768ca7
sabbir <sabbir@gmail.com>
2022-11-08 19:52:26 +0600

    sihn in with google

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,6 +1,23 @@
+import { GoogleAuthProvider } from 'firebase/auth';
 import React from 'react';
+import { useContext } from 'react';
+import { FaGoogle } from 'react-icons/fa';
+import { AuthContext } from '../../Context/AuthProvider/AuthProvider';
 
 const Login = () => {
+
+    const { signWithGoogle } = useContext(AuthContext);
+    const googleProvider = new GoogleAuthProvider();
+
+    const handleWithGoogle = () => {
+
+        signWithGoogle(googleProvider)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+            }).catch(err => console.error(err));
+    };
+
     return (
         <div className='container mx-auto p-10 bg-slate-700'>
             <form >
@@ -9,6 +26,9 @@ const Login = () => {
                 <input type="text" placeholder="password" className="input input-bordered w-full " />
                 <button type='submit' className='btn mt-4'>Submit</button>
             </form>
+            <div className='text-center my-2 text-white text-2xl'>
+                <button onClick={handleWithGoogle} className='btn'>  <FaGoogle > </FaGoogle> <span className='ml-1'>SignIn Wth Google </span> </button>
+            </div>
         </div>
     );
 };
