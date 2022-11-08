c5d8181e070ecc573f20974d5094ea6931ec71c6
sabbir <sabbir@gmail.com>
2022-11-09 00:05:31 +0600

    review section added

--- src/Pages/ServiceDeatails/ReviewSection.js
+++ src/Pages/ServiceDeatails/ReviewSection.js
@@ -1,9 +1,60 @@
 import React from 'react';
+import { useContext } from 'react';
+import { AuthContext } from '../../Context/AuthProvider/AuthProvider';
 
 const ReviewSection = ({ service }) => {
-    return (
-        <div>
 
+
+    const { user } = useContext(AuthContext);
+    const { title, _id } = service;
+
+    const handlePlaceOrder = (event) => {
+
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const photoURL = form.photoURL.value;
+        const email = user?.email || 'unregistered';
+        const message = form.message.value;
+
+        const review = {
+            serviceName: title,
+            servicesId: _id,
+            tourist: name,
+            email,
+            photoURL,
+            message
+        };
+        console.log(review);
+
+        fetch('http://localhost:5000/review', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(review)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+
+            }).catch(er => console.error(er));
+
+    };
+
+    return (
+        <div className='container mx-auto my-10'>
+            <h2 className="text-4xl">You are about to order: <span className='font-bold text-yellow-900'>{title}</span></h2>
+            <form className='p-4' onSubmit={handlePlaceOrder}>
+                <div className='grid grid-cols-1 lg:grid-cols-2 gap-4'>
+                    <input name="name" type="text" placeholder="Full Name" className="input input-ghost w-full  input-bordered" required />
+                    <input name="photoURL" type="text" placeholder="photoURL" className="input input-ghost w-full  input-bordered" required />
+                    <input name="email" type="text" placeholder="Your email" defaultValue={user?.email} className="input input-ghost w-full  input-bordered" readOnly />
+                </div>
+                <textarea name="message" className="textarea textarea-bordered h-24 w-full my-4" placeholder="Your Message" required></textarea>
+
+                <input className='btn' type="submit" value="Give a Review" />
+            </form>
         </div>
     );
 };
--- src/Pages/ServiceDeatails/ServiceDeatails.js
+++ src/Pages/ServiceDeatails/ServiceDeatails.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { useContext } from 'react';
+import { Link, useLoaderData } from 'react-router-dom';
+import { AuthContext } from '../../Context/AuthProvider/AuthProvider';
+import Login from '../Login/Login';
 import ReviewSection from './ReviewSection';
 import ServiceSection from './ServiceSection';
 
 const ServiceDeatails = () => {
 
+    const { user } = useContext(AuthContext);
     const service = useLoaderData();
 
     return (
         <div>
             <ServiceSection service={service}></ServiceSection>
-            <ReviewSection service={service}></ReviewSection>
+            {
+                user?.uid ?
+                    <ReviewSection service={service}></ReviewSection> :
+                    <h2 className='text-4xl text-center my-10 font-semibold text-yellow-900'>You are not Login. To give a review please <Link to='/login' className='underline text-primary'>Login</Link></h2>
+            }
         </div>
     );
 };
